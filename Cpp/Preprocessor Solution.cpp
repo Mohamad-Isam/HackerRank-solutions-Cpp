156cf999067205e9ca44b59be7d230d6dc881b29
Mohamed Isam <116897631+Train-XIII@users.noreply.github.com>
2023-02-02 18:51:10 +0200

    Create Preprocessor Solution.cpp

---
+++ Cpp/Preprocessor Solution.cpp
@@ -0,0 +1,6 @@
+/* Enter your macros here */
+#define toStr(x) #x
+#define io(s) cin >> s
+#define foreach(v, i) for(int i = 0; i < v.size(); i++)
+#define FUNCTION(name, op) void name(int &x, int &y){if(y op x) x = y;}
+#define INF 1e9

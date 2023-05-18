4f7b1eb3764fd57753bde3f4822050b7f7a3ccd6
Mohamed Isam <116897631+Mohamad-Isam@users.noreply.github.com>
2023-05-18 17:17:42 +0200

    Create The Longest Common Subsequence.cpp

---
+++ algorithm./Dynamic Programming/The Longest Common Subsequence.cpp
@@ -0,0 +1,147 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'longestCommonSubsequence' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+vector<int> LCS(vector<int> X, vector<int> Y, int m, int n) {
+        vector<vector <int>> dp(m + 1, vector<int> (n + 1));
+        for(int i = 0; i <= m; i++)
+        {
+            for(int j = 0; j <= n; j++)
+            {
+                if(i == 0 || j == 0)
+                    dp[i][j] = 0;
+                else if(X[i - 1] == Y[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        
+        int index = dp[m][n];
+        vector<int> result(index);
+        int i = m, j = n;
+        while(i > 0 && j > 0)
+        {
+            if(X[i - 1] == Y[j - 1])
+            {
+                result[index - 1] = X[i - 1];
+                --i;
+                --j;
+                --index;
+            }
+            else if(dp[i - 1][j] > dp[i][j - 1])
+                --i;
+            else
+                --j;
+        }
+        
+        return result;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int m = stoi(first_multiple_input[1]);
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split(rtrim(a_temp_temp));
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    string b_temp_temp;
+    getline(cin, b_temp_temp);
+
+    vector<string> b_temp = split(rtrim(b_temp_temp));
+
+    vector<int> b(m);
+
+    for (int i = 0; i < m; i++) {
+        int b_item = stoi(b_temp[i]);
+
+        b[i] = b_item;
+    }
+
+    vector<int> result = LCS(a, b, a.size(), b.size());
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
